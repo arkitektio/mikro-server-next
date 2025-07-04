8a9b634bda79cd370197e634f0f3c725dd2c84d4
Johannes Roos <jhnnsrs@gmail.com>
2025-07-04 14:46:24 +0200

    fix: trigger bulid and add secret key

--- mikro_server/settings.py
+++ mikro_server/settings.py
@@ -23,7 +23,7 @@ conf = OmegaConf.load(os.path.join(BASE_DIR, "config.yaml"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-6vh8x**%4mm0yxjbghipsalf5$wum10_satqhxg$vo9jninehx"
+SECRET_KEY = conf.django.get("secret_key", "changeme")  # TODO: Change this in production
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
