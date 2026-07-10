4d99afb5c3c47b5ddcd7839817c5f9739717877a
Johannes Roos <jhnnsrs@gmail.com>
2026-07-10 15:36:58 +0200

    fix: unique together

--- core/models/instrumentation.py
+++ core/models/instrumentation.py
@@ -6,7 +6,7 @@ from authentikate.models import Organization
 
 
 class Objective(models.Model):
-    serial_number = models.CharField(max_length=1000, unique=True)
+    serial_number = models.CharField(max_length=1000)
     name = models.CharField(max_length=1000)
     magnification = models.FloatField(blank=True, null=True)
     na = models.FloatField(blank=True, null=True)
@@ -20,10 +20,13 @@ class Objective(models.Model):
         help_text="The users that have pinned the objective",
     )
 
+    class Meta:
+        unique_together = ("serial_number", "organization")
+
 
 class Camera(models.Model):
-    serial_number = models.CharField(max_length=1000, unique=True)
-    name = models.CharField(max_length=1000, unique=True)
+    serial_number = models.CharField(max_length=1000)
+    name = models.CharField(max_length=1000)
     model = models.CharField(max_length=1000, blank=True, null=True)
     bit_depth = models.IntegerField(blank=True, null=True)
     sensor_size_x = models.IntegerField(blank=True, null=True)
@@ -40,12 +43,15 @@ class Camera(models.Model):
         help_text="The users that have pinned the camera",
     )
 
+    class Meta:
+        unique_together = ("serial_number", "organization")
+
 
 class Instrument(models.Model):
     name = models.CharField(max_length=1000)
     manufacturer = models.CharField(max_length=1000, null=True, blank=True)
     model = models.CharField(max_length=1000, null=True, blank=True)
-    serial_number = models.CharField(max_length=1000, unique=True)
+    serial_number = models.CharField(max_length=1000)
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
     provenance = ProvenanceField()
     pinned_by = models.ManyToManyField(
@@ -54,3 +60,6 @@ class Instrument(models.Model):
         blank=True,
         help_text="The users that have pinned the instrument",
     )
+
+    class Meta:
+        unique_together = ("serial_number", "organization")
